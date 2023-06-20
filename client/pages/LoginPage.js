60db9e631b70af1855ea8858863cbbc124b7d5cf
CO <99570414+codeFromCO@users.noreply.github.com>
2023-06-20 18:53:00 -0400

    LoginPage updated however axios.get will need review

--- client/pages/LoginPage.js
+++ client/pages/LoginPage.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axois from 'axios';
+
+
 export default function LoginPage() {
+    
+    const [loginUserVal, setLoginUserVal] = useState("");
+    const [loginPassVal, setLoginPassVal] = useState("");
+
+    async function login() {
+        console.log("loginUserVal:", loginUserVal)
+        // '/api/user/login' functionality not yet written 
+        const response = await axois.get('/api/user/login', {
+            username: loginUserVal,
+            password: loginPassVal
+        })
+        console.log("login:", response)
+    };
+    
     return (
-        <div>Login Page</div>
+        <div>
+            <h1>Log In </h1>
+            <label>Username</label>
+            <input id="username" value={loginUserVal} onChange={(e) => setLoginUserVal(e.target.value)}/>
+            <label>Password</label>
+            <input value ={loginPassVal}  onChange={(e) => setLoginPassVal(e.target.value)} />
+            <button id="login-button" onClick={login}>Log In</button>
+        </div>
     )
 }
